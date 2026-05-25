0c05f895e399a4a86a765456479d1cfb88988b0e
parjlp <110988133+parjlp@users.noreply.github.com>
2026-05-25 18:43:49 +0100

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -76,7 +76,7 @@ def load_data():
         'Through passes per 90', 'Accurate through passes, %',
         'Shot assists per 90', 'Second assists per 90', 'Deep completions per 90',
         'Touches in box per 90', 'Offensive duels per 90', 'Offensive duels won, %',
-        'Role', 'Role''Division', 'Due For Release'
+        'Role', 'Role', 'Division', 'Due For Release'
     ]
 
     forward_cols = [
@@ -92,7 +92,7 @@ def load_data():
         'Touches in box per 90', 'Progressive runs per 90', 'Accelerations per 90',
         'Fouls suffered per 90', 'Received passes per 90',
         'Key passes per 90', 'Passes to penalty area per 90', 'Accurate passes to penalty area, %',
-        'Role', 'Role''Division', 'Due For Release'
+        'Role', 'Role', 'Division', 'Due For Release'
     ]
 
     goalkeeper_cols = [
@@ -108,7 +108,7 @@ def load_data():
         'Passes per 90', 'Accurate passes, %',
         'Long passes per 90', 'Accurate long passes, %',
         'Average pass length, m', 'Average long pass length, m',
-        'Role', 'Role''Division', 'Due For Release'
+        'Role', 'Role', 'Division', 'Due For Release'
     ]
 
     exclude_cols = [
